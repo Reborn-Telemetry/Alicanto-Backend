6a84f4b329d92f5d5cfdc348f13134848a46e6f4
hurbx <hugo.urbina@mayor.cl>
2024-01-22 15:38:58 -0300

    profile view, bus a db

--- bus_signals/urls.py
+++ bus_signals/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
   path("bus_list/", views.bus_list, name="bus_list"),
   path("dic_fusi/", views.dic_fusi, name="dic_fusi"),
   path("odometer/", views.odometer, name="odometer"),
+  path("bus_detail/<str:pk>/", views.bus_detail, name="bus_detail"),
     
 ]
