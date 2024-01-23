ba06ead4d1ff0d72bffbfb75834f70812947f097
hurbx <hugo.urbina@mayor.cl>
2024-01-23 14:27:33 -0300

    create fusi incomplete

--- bus_signals/urls.py
+++ bus_signals/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
   path("odometer/", views.odometer, name="odometer"),
   path("bus_detail/<str:pk>/", views.bus_detail, name="bus_detail"),
   path("create_bus/", views.create_bus, name="create_bus"),
+  path("create_fusi/", views.create_fusi, name="create_fusi"),
   path("update_bus/<str:pk>/", views.update_bus, name="update_bus"),
   path("delete_bus/<str:pk>/", views.delete_bus, name="delete_bus")
     
