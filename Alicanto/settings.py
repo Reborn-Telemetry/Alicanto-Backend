0063a8ee210a000cfdb0871c7aa73065f8c4557e
hurbx <hugo.urbina@mayor.cl>
2024-01-26 16:04:45 -0300

    solucion problema migraciones - nueva db implementada - migraciones realizadas a la nueva db

--- Alicanto/settings.py
+++ Alicanto/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'Alicanto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alicanto-db-dev',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'alicanto-db-v1.cyydo36bjzsy.us-west-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
