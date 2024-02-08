f8bf6888467b8a6f610d8cedc0d376c85b9096c3
hurbx <hugo.urbina@mayor.cl>
2024-02-08 11:44:25 -0300

    login system v1 finish

--- bus_signals/urls.py
+++ bus_signals/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("", views.login, name="login"),
+  path("", views.login_page, name="login"),
   path("bus_list/", views.bus_list, name="bus_list"),
   path("dic_fusi/", views.dic_fusi, name="dic_fusi"),
   path("odometer/", views.odometer, name="odometer"),
