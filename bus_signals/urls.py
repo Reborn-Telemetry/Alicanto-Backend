9a89365d3bbf5bba126968411ff41f8d9f07cdb7
hurbx <hugo.urbina@mayor.cl>
2024-02-26 09:31:42 -0300

    orden templates y urls

--- bus_signals/urls.py
+++ bus_signals/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
   path("logout_user/", views.logout_user, name="logout_user"),
   path("bus_list/", views.bus_list, name="bus_list"),
   path("dic_fusi/", views.dic_fusi, name="dic_fusi"),
-  path("odometer/", views.odometer, name="odometer"),
   path("bus_detail/<str:pk>/", views.bus_detail, name="bus_detail"),
   path("create_bus/", views.create_bus, name="create_bus"),
   path("create_fusi/", views.create_fusi, name="create_fusi"),
