03b213576348b5fd0e34ac6aa99f2b79b8c67fc4
hurbx <hugo.urbina@mayor.cl>
2024-03-25 14:35:20 -0300

    url fix profiles

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile, name='profile'),
+    path('profiles', views.profile, name='profile'),
     path('work_order/', views.create_work_order, name='work_order_form'),
     path('update_ot/<str:pk>/', views.update_ot, name='update_ot'),
 
