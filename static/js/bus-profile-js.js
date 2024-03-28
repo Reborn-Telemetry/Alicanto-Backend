1fab795b30732b7060a2ca57eec212ec05e48c19
hurbx <hugo.urbina@mayor.cl>
2024-03-27 22:50:46 -0300

    cambios perfil bus

---
+++ static/js/bus-profile-js.js
@@ -0,0 +1,11 @@
+$(function () {
+    $(".chart").easyPieChart({
+        size: 160,
+        barColor: "#7ADA00",
+        scaleLength: 0,
+        lineWidth: 15,
+        trackColor: "#EDEDED",
+        lineCap: "circle",
+        animate: 2000,
+    });
+});
