9b2b62f6705468e634c9995ada02d583edc4ca98
hurbx <hugo.urbina@mayor.cl>
2024-04-01 11:21:39 -0300

    modelos disponbilidad

--- reports/models.py
+++ reports/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+series_choices = (
+    ('Queltehue', 'Queltehue'),
+    ('Queltehue-Q2', 'Queltehue-Q2'),
+    ('Tricahue', 'Tricahue'),
+    ('Retrofit', 'Retrofit'),
+)
+
+class DisponibilidadFlota(models.Model):
+  bus= models.CharField(max_length=50),
+  fecha = models.DateField(auto_now_add=True, blank=True, null=True),
+  disponibilidad = models.BooleanField(default=True, blank=True, null=True),
+  serie = models.CharField(max_length=50, choices=series_choices, blank=True, null=True),
+  observacion = models.TextField(blank=True, null=True),
+  dias_operativos = models.IntegerField(blank=True, null=True),
+  dias_fs = models.IntegerField(blank=True, null=True),
+
+  disponibilidad = models.Manager()
+
+  def __str__(self):
+    return f'{self.bus} - {self.fecha} - {self.disponibilidad} - {self.serie}'
+  
+  class Meta:
+    verbose_name = 'Disponibilidad de Flota'
+    verbose_name_plural = 'Disponibilidad de Flota'
+   
+
+ 
+
--- reports/views.py
+++ reports/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from bus_signals.models import Bus
+
+import requests
+no_update_list = ['27','34', '60', '24', '87', '116', '21', '61', '82', '83', '81']
 
 # Create your views here.
+def disponbilidad_flota(request):
+   headers = { 'User-Agent': 'Alicanto/1.0', }
+   api_url = 'https://reborn.assay.cl/api/v1/fs_elec'
+   response = requests.get(api_url, headers=headers)
+   data = response.json()
+   bus_fs = []
+   for i in data['data']:
+      bus_fs.append(i['vehicle'].capitalize())
+
+   bus_operativo = Bus.bus.all().exclude(bus_name__in=bus_fs)
+   bus_operativo = bus_operativo.exclude(id__in=no_update_list)
+
+   
+
+   print(bus_fs) 
+   print(bus_operativo)
+   
+
+   
+   
+
+   return render(request, 'reports/disponibilidad_flota.html')
