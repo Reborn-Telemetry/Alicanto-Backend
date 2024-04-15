17a062431994ba0c271f5bbfba307f0614b0a6b1
hurbx <hugo.urbina@mayor.cl>
2024-04-15 15:36:11 -0400

    latbar new

--- static/js/latbar.js
+++ static/js/latbar.js
@@ -1,21 +1,7 @@
-const body = document.querySelector("body"),
-    sidebar = body.querySelector("nav"),
-    toggle = body.querySelector(".toggle"),
-    searchBtn = body.querySelector(".search-box"),
-    modeSwitch = body.querySelector(".toggle-switch"),
-    modeText = body.querySelector(".mode-text");
-toggle.addEventListener("click", () => {
-    sidebar.classList.toggle("close");
-});
-searchBtn.addEventListener("click", () => {
-    sidebar.classList.remove("close");
-});
-modeSwitch.addEventListener("click", () => {
-    body.classList.toggle("dark");
-
-    if (body.classList.contains("dark")) {
-        modeText.innerText = "Light mode";
-    } else {
-        modeText.innerText = "Dark mode";
-    }
-});
+(function () {
+    "use strict";
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+        new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+})();
