96334dff1dc723b82871bc8edb697983b94d9a68
hurbx <hugo.urbina@mayor.cl>
2024-05-14 16:51:27 -0400

    cambio numero mes x nombre

--- reports/views.py
+++ reports/views.py
@@ -27,7 +27,9 @@ def historical_data(request):
      if request.method == 'GET':
         context = {
             'bus': Bus.bus.all().exclude(id__in=no_update_list),
-            'meses': ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+            'meses2': [{'mes':'Enero', 'numero':1}, {'mes':'Febrero', 'numero':2},{'mes':'Marzo', 'numero':3}, {'mes':'Abril', 'numero':4},
+                       {'mes':'Mayo', 'numero':5}, {'mes':'Junio', 'numero':6}, {'mes':'Julio', 'numero':7}, {'mes':'Agosto', 'numero':8},
+                       {'mes':'Septiembre', 'numero':9}, {'mes':'Octubre', 'numero':10}, {'mes':'Noviembre', 'numero':11}, {'mes':'Diciembre', 'numero':12}],
         }
       
         # Lógica para manejar la solicitud GET
