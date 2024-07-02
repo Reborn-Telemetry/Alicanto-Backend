8f23fdf1513d2faf6e4a4076dcbf7b70b5d8cebf
hurbx <hugo.urbina@mayor.cl>
2024-07-02 10:06:56 -0400

    kwh comment

--- bus_signals/views.py
+++ bus_signals/views.py
@@ -518,7 +518,7 @@ def dashboard(request):
         linechart_data.append({'month': month, 'total': round(total * 0.00067)})
         linechart_data2.append({'month': month, 'total': round(total * 0.0004187)})
 
-    # energia total cargada año
+        """# energia total cargada año
     energia_anual = 0
     for i in Bus.bus.all():
         charge_data = ChargeStatus.charge_status.filter(bus_id=i.id).order_by('TimeStamp')
@@ -595,7 +595,9 @@ def dashboard(request):
         acu = round(sum(i['energia'] for i in datos_tabla), 2)
         energia_anual += acu
         energia_anual = round(energia_anual)
-        request.session['energia_anual'] = round(energia_anual)
+        request.session['energia_anual'] = round(energia_anual)"""
+
+    
         request.session['charging'] = charging
 
     context = {
@@ -614,7 +616,7 @@ def dashboard(request):
         'linechart_data': linechart_data,
         'linechart_data2': linechart_data2,
         'charging': charging,
-        'energia_anual': energia_anual,
+        #'energia_anual': energia_anual,
         'fs_vehicles': fs_vehicles,
     }
     return render(request, 'pages/dashboard.html', context)
