c7ab5e35949a7434dd3fff2594a5e99f1322b06c
hurbx <hugo.urbina@mayor.cl>
2024-09-06 09:31:48 -0400

    api_link_fix

--- services/fs_link.py
+++ services/fs_link.py
@@ -1,6 +1,8 @@
 import requests
 def fs_link_api():
-   headers = {'User-Agent': 'Alicanto/1.0',}
+   headers = {'User-Agent': 'Alicanto/1.0',
+              'Connection': 'keep-alive',
+              }
    api_url = 'https://reborn.assay.cl/api/v1/fs_elec'
    response = requests.get(api_url, headers=headers)
    data = response.json()
