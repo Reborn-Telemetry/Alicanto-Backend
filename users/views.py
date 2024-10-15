57c6b1a691bb1bdf5229e9a495c9a2f412261a18
hurbx <hugo.urbina@mayor.cl>
2024-10-15 15:20:07 -0300

    thread nuevo modelo kwh anual creado

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from bus_signals.models import Bus, ChargeStatus
+from bus_signals.models import Bus, ChargeStatus, Odometer
 from bus_signals.query_utils import obtener_ultimo_valor_energia
+from django.db.models import F, Max, Window
+from django.db.models.functions import ExtractMonth, ExtractDay
 # Create your views here.
 from django.shortcuts import render, redirect
 from bus_signals.forms import WorkOrderForm
@@ -11,6 +13,9 @@ import pytz
 from django.utils import timezone
 from collections import defaultdict
 from datetime import datetime, timedelta
+from django.db.models import F, Case, When, IntegerField, Window
+from django.db.models.functions import RowNumber
+from itertools import groupby
 no_update_list = [ '27','34', '60', '24', '87', '116', '21', '61', '82', '83', '81', '87', '137', '132', '134', '133', '130', '129', '128', '131', '136', '135' ]
 # Create your views here.
 @login_required(login_url='login')
@@ -46,124 +51,64 @@ def update_ot(request, pk):
     context = {'form': form}
     return render(request, 'users/work_order_form.html', context)
 
+def format_date(date):
+    return date.strftime('%d/%m/%Y')  # Formato: día/mes/año
 
 def energy_record(request):
-    energia_cargada_flota = 0
-    days_of_month = range(1, 32) 
-    lista_datos_organizados = [] 
-    for y in Bus.bus.all().exclude(id__in=no_update_list):
-        charge_data = ChargeStatus.charge_status.filter(bus_id=y.id).order_by('TimeStamp')
 
-        rangos = []
-        rango_actual = []
+    #----------buses cargando----------------------------------
+ charging = request.session.get('charging')
+ energia_anual = request.session.get('energia_anual')
+    #---------------------------------------------------------
+   
+ bus_id = 1  # Cambia por el ID del bus que deseas filtrar
+ mes = 10    # Cambia por el mes que deseas (número de mes)
 
-        for item in charge_data:
-            if item.charge_status_value == 1:
-                rango_actual.append(item)
-            elif item.charge_status_value == 0:
-                if rango_actual:
-                    rangos.append(rango_actual.copy())
-                    rango_actual.clear()
-            else:
-                continue
+# Obtener los registros de ChargeStatus filtrados por bus_id y mes, y ordenados por TimeStamp
+ charge_statuses = ChargeStatus.charge_status.filter(bus_id=bus_id).annotate(
+    mes=ExtractMonth('TimeStamp')
+).filter(mes=mes).order_by('TimeStamp')
 
-    # Agregar el último rango si no termina con Estado 0.0
-        if rango_actual:
-            rangos.append(rango_actual)
+# Lista para almacenar ciclos de carga
+ charge_cycles = []
+ current_cycle = []
 
-        santiago_tz = pytz.timezone('Chile/Continental')
-        
 
-    # Preparar los datos para la tabla y calcular acumulados
-        datos_tabla = []
-        for i, rango in enumerate(rangos, 1):
-            fecha_inicio = rango[0].TimeStamp.strftime("%Y-%m-%d %H:%M:%S")
-            fecha_termino = rango[-1].TimeStamp.strftime("%Y-%m-%d %H:%M:%S")
-            soc_inicial = rango[0].soc_level
-            soc_final = rango[-1].soc_level
-            carga = soc_final - soc_inicial  # Resta de soc_level
+# Iterar sobre los registros y agrupar por ciclos continuos de valores 1
+ for status in charge_statuses:
+    if status.charge_status_value == 1:
+        current_cycle.append(status)
+    else:
+        # Si no es 1, y tenemos un ciclo en curso, lo agregamos a la lista de ciclos
+        if current_cycle:
+            first_status = current_cycle[0]
+            last_status = current_cycle[-1]
+            soc_difference = last_status.soc_level - first_status.soc_level  # Diferencia de SOC
 
-            fecha_inicio_dt = datetime.strptime(fecha_inicio, '%Y-%m-%d %H:%M:%S')
-            fecha_termino_dt = datetime.strptime(fecha_termino, '%Y-%m-%d %H:%M:%S')
-
-            fecha_inicio_dt_santiago = fecha_inicio_dt.replace(tzinfo=pytz.utc).astimezone(santiago_tz)
-            fecha_termino_dt_santiago = fecha_termino_dt.replace(tzinfo=pytz.utc).astimezone(santiago_tz)
-
-        # Calcular la diferencia de tiempo en horas
-            diferencia = fecha_termino_dt_santiago - fecha_inicio_dt_santiago
-            diferencia_en_horas = diferencia.total_seconds() / 3600
-
-            datos_tabla.append({
-                'rango': i,
-                'fecha_inicio': fecha_inicio_dt_santiago.strftime("%Y-%m-%d %H:%M:%S"),
-                'fecha_termino': fecha_termino_dt_santiago.strftime("%Y-%m-%d %H:%M:%S"),
-                'tiempo': round(diferencia_en_horas, 2),
-                'soc_inicial': soc_inicial,
-                'soc_final': soc_final,
-                'carga': carga,
-                'energia': (carga * 140) / 100,
-                'bus': y.bus_name
+            charge_cycles.append({
+                'fecha_inicio': format_date(first_status.TimeStamp),
+                'fecha_final': format_date(last_status.TimeStamp),
+                'diferencia_carga': soc_difference,
+                'energia': soc_difference  # Si la energía es igual a la diferencia de carga
             })
+            current_cycle = []  # Reiniciar el ciclo
 
-        acumulado_mensual = {str(month).zfill(2): 0 for month in range(1, 13)}  
+# Agregar el último ciclo si terminó con un 1
+ if current_cycle:
+    first_status = current_cycle[0]
+    last_status = current_cycle[-1]
+    soc_difference = first_status.soc_level - last_status.soc_level  # Diferencia de SOC
 
-        fecha_actual = datetime.now()
+    charge_cycles.append({
+        'fecha_inicio': format_date(first_status.TimeStamp),
+        'fecha_final': format_date(last_status.TimeStamp),
+        'diferencia_carga': soc_difference,
+        'energia': soc_difference  # Si la energía es igual a la diferencia de carga
+    })
 
-        # Obtener el primer día del mes y el último día del mes actual
-        primer_dia_mes = datetime(fecha_actual.year, fecha_actual.month, 1)
-        ultimo_dia_mes = datetime(fecha_actual.year, fecha_actual.month + 1, 1) - timedelta(days=1)
+# La lista charge_cycles ahora contiene los diccionarios con la información de cada ciclo formateada
+ for cycle in charge_cycles:
+    print(cycle)
+ context = {'charging': charging, 'energia_anual':energia_anual}
 
-    # Crear una lista con todas las fechas del mes
-        dias_mes = [primer_dia_mes + timedelta(days=d) for d in range((ultimo_dia_mes - primer_dia_mes).days + 1)]
-
-    # Inicializar la tabla de energía con todas las fechas del mes y energía total en cero
-        tabla_energia = [{'bus':y.bus_name, 'fecha': fecha.strftime('%Y-%m-%d'), 'energia_total': 0} for fecha in dias_mes]
-        complete_table = []
-
-    # Actualizar la energía total en la tabla con los valores calculados
-        for item in tabla_energia:
-            for dato in datos_tabla:
-                if dato['fecha_inicio'][:10] == item['fecha']:
-                    item['energia_total'] += (dato['carga'] * 140) / 100
-        
-    
-    
-        for item in tabla_energia:
-                bus = item['bus']
-                fecha = item['fecha']
-                energia_total = item['energia_total']
-
-                bus_existe = False
-                for datos_bus in lista_datos_organizados:
-                    if datos_bus['bus'] == bus:
-                        energia_total_formateada = "{:.1f}".format(energia_total)
-                        datos_bus['datos'].append({'fecha': fecha, 'energia_total': energia_total_formateada})
-                        bus_existe = True
-                        energia_cargada_flota += energia_total
-                        break
-                if not bus_existe:
-                    lista_datos_organizados.append({'bus': bus, 'datos': [{'fecha': fecha, 'energia_total': round(energia_total,2)}]})
-                
-    ultimo_valor_energia = obtener_ultimo_valor_energia(lista_datos_organizados)
-    lista_ultimo_valor_energia = [{'bus': bus, 'energia': energia} for bus, energia in ultimo_valor_energia.items()]
-    grafico = list(lista_ultimo_valor_energia)
-    energia_anual = request.session.get('energia_anual')
-    charging = request.session.get('charging')
-
-   
-
-   
-        
-
-    context = {
-                    'lista_datos_organizados': lista_datos_organizados,
-                    'days_of_month': days_of_month,
-                    'energia_cargada_flota': round(energia_cargada_flota,2),
-                    'ultimo_valor_energia': grafico,
-                    'energia_anual': energia_anual,
-                    'charging': charging
-                    }
-
-    # Imprimir la tabla de energía completa para el mes actual
-
-    return render(request, 'reports/energy-record.html', context)
+ return render(request, 'reports/energy-record.html', context)
