2bca2709ee12b8091f3d40472dc58a2e8a5cc9cb
hurbx <hugo.urbina@mayor.cl>
2024-10-15 15:53:30 -0300

    api buses nueva app

--- Alicanto/urls.py
+++ Alicanto/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("bus_signals.urls")),
+    path('api/', include("link_apis.urls")),
     path('', include("users.urls")),
     path('', include("reports.urls")),
 ]
