7bfd2993811bb8a7e9a16af3e9f48867389bf272
hurbx <hugo.urbina@mayor.cl>
2024-10-16 11:30:13 -0300

    cron job thread

--- bus_signals/apps.py
+++ bus_signals/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
 
 
+
 class BusSignalsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bus_signals'
+
+    def ready(self):
+        from bus_signals.threads.energia_anual import iniciar_calculo_diario
+        iniciar_calculo_diario()
