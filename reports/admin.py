f18f84250ad864641d9de70fd860c785500e011f
hurbx <hugo.urbina@mayor.cl>
2024-10-23 14:19:47 -0300

    register new models

--- reports/admin.py
+++ reports/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import DisponibilidadFlota
+from .models import DisponibilidadFlota, MatrizEnergiaFlotaHistorico, DailyMatrizKmAutoReport
 admin.site.register(DisponibilidadFlota)
+admin.site.register(MatrizEnergiaFlotaHistorico)
+admin.site.register(DailyMatrizKmAutoReport)
 
 # Register your models here.
