ca7b0ed4e2cace5da91ae9913d3c3ba7332fcf8b
hurbx <hugo.urbina@mayor.cl>
2024-10-24 12:31:14 -0300

    cron test 4

--- bus_signals/threads/energia_anual.py
+++ bus_signals/threads/energia_anual.py
@@ -1,6 +1,7 @@
 from pytz import timezone
 from datetime import datetime
 from reports.models import MatrizEnergiaFlotaHistorico
+from bus_signals.models import AnualEnergy  # Asegúrate de importar tu modelo AnualEnergy
 from django.db.models import Sum
 from apscheduler.triggers.cron import CronTrigger
 import logging
@@ -23,16 +24,19 @@ def calcular_energia_anual_diaria():
     year = datetime.now().year
     # Llamar a la función con el año actual
     total_energy = calcular_energia_anual(year)
-    
+
+    # Guardar la energía calculada en el modelo AnualEnergy
+    AnualEnergy.objects.create(energia=total_energy)
+
     # Log para verificar la ejecución
     logging.info(f"Energía total calculada para el año {year}: {total_energy}")
 
 
 def iniciar_calculo_diario(scheduler):
-    # Configurar el trigger para que se ejecute todos los días a las 11 AM hora de Chile
+    # Configurar el trigger para que se ejecute todos los días a las 12:30 PM hora de Chile
     scheduler.add_job(
         calcular_energia_anual_diaria,  # La función que se ejecutará diariamente
-        trigger=CronTrigger(hour=12, minute=30, timezone=timezone("America/Santiago")),
+        trigger=CronTrigger(hour=12, minute=40, timezone=timezone("America/Santiago")),
         id="calcular_energia_anual_diaria",
         replace_existing=True,
     )
