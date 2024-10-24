f6388520df93e9d80a7f43e87441938ddd4324f1
hurbx <hugo.urbina@mayor.cl>
2024-10-24 11:59:19 -0300

    energia anual fix cron test

--- reports/apps.py
+++ reports/apps.py
@@ -1,15 +1,14 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_migrate
 
 class ReportsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reports'
 
     def ready(self):
-        # Conectar la señal post_migrate para iniciar el scheduler después de las migraciones
-        post_migrate.connect(self.start_scheduler, sender=self)
+        # Iniciar el scheduler directamente cuando la aplicación esté lista
+        self.start_scheduler()
 
-    def start_scheduler(self, **kwargs):
+    def start_scheduler(self):
         from apscheduler.schedulers.background import BackgroundScheduler
         from django_apscheduler.jobstores import DjangoJobStore
 
@@ -17,7 +16,7 @@ class ReportsConfig(AppConfig):
         from bus_signals.threads.matriz_energia_historico_flota import iniciar_calculo_historico_diario
         from bus_signals.threads.max_odometer_dayly import iniciar_calculo_odometro_diario
 
-        # Inicializar el scheduler una sola vez dentro de la señal post_migrate
+        # Inicializar el scheduler una sola vez
         scheduler = BackgroundScheduler()
         scheduler.add_jobstore(DjangoJobStore(), "default")
 
